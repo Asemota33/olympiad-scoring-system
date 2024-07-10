b1024196c606e21e0d754fc0796104e854bc5b62
Michael Asemota <michaelasemota1@hotmail.com>
2024-07-09 22:52:09 -0400

    Added data grid and text for scoreboard component

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
   styleUrl: './scoreboard.component.scss'
 })
-export class ScoreboardComponent {
-
+export class ScoreboardComponent implements OnInit{
+  
+  users: any;
+  
+  ngOnInit(): void {
+    this.users = [
+      {
+        id: "1",
+        name: "michael",
+        creation: "value",
+        color: "value2"
+      }
+    ]
+  }
+  
 }
