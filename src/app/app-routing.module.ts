7214aefc539c8bb077168a049443cd015a7a9adb
Michael Asemota <michaelasemota1@hotmail.com>
2024-12-20 19:06:01 -0500

    Fixed nav and added mobile view of overview page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   },
 
   {path: '', redirectTo: '/scoreboard', pathMatch: 'full', },
-  {path: '**', component: MatchupsComponent},
+  {path: '**', redirectTo: '/scoreboard', pathMatch: 'full'},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'olympiad-scoring-system';
+  navDetails: Array<any>
+  clickedContent = ""
+  isSmallScreen = false;
+
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+    this.navDetails = [
+      { name: "Overview", path: "/scoreboard"},
+      { name: "Rankings", path: "/points"},
+      { name: "Matchup Generation", path: "/matchups"},
+      { name: "Score Progression", path: "#"},
+    ]
+    
+  }
+
+  ngOnInit(): void {
+    const currentRoute = document.location.pathname
+    const activeLink = this.navDetails.find(link => link.path === currentRoute);
+
+    if (activeLink) {
+      this.clickedContent = activeLink.name;
+    } else {
+      this.clickedContent = 'Overview'; 
+    }
+
+    
+    this.breakpointObserver.observe(['(max-width: 768px)']).subscribe(result => {
+      console.log("result: ", result)
+      this.isSmallScreen = result.matches;
+    });
+  }
+
+  updateTitle(event: any) {
+    const target = event.target as HTMLElement
+    this.clickedContent = target.innerText
+  }
 }
