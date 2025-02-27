46604036cdb5f9761390564b5687cf8fd8ea9eb4
Michael Asemota <michaelasemota1@hotmail.com>
2025-02-26 20:24:38 -0500

    Moved overview cards to score and rankings and table from score and rankings to overview

--- src/app/points-scoreboard/points-scoreboard.component.ts
+++ src/app/points-scoreboard/points-scoreboard.component.ts
@@ -12,88 +12,15 @@ import { ScoreboardService } from '../services/scoreboard.service';
   styleUrl: './points-scoreboard.component.scss',
 })
 export class PointsScoreboardComponent {
-  gameRankings?: any[];
-  columnHeader: string[] = [];
-  dataReady: Observable<boolean> | undefined;
-  displayedColumns: any;
-  columns: any[];
-  dataSource?: any[];
-  contentReady = false;
 
-  constructor(private scoreboardService: ScoreboardService) {
-    setTimeout(() => {
-      this.scoreboardService.getTeamPoints().subscribe(teamResults => {
-        this.gameRankings = teamResults;
-        this.dataSource = this.transformedTeams(this.gameRankings)
-        this.contentReady = true;
-      })
-    }, 1000);
 
-    
 
-    this.columns = [
-      {
-        columnDef: 'position',
-        header: '#',
-        cell: (element: any) => `${element.position}`,
-      },
-      {
-        columnDef: 'name',
-        header: 'Team',
-        cell: (element: any) => `${element.name}`,
-      },
-      {
-        columnDef: 'numOfFirsts',
-        header: 'Firsts',
-        cell: (element: any) => `${element.firsts}`,
-      },
-      {
-        columnDef: 'numOfSeconds',
-        header: 'Seconds',
-        cell: (element: any) => `${element.seconds}`,
-      },
-      {
-        columnDef: 'numOfThirds',
-        header: 'Thirds',
-        cell: (element: any) => `${element.thirds}`,
-      },
-      {
-        columnDef: 'numOfFourths',
-        header: 'Fourths',
-        cell: (element: any) => `${element.fourths}`,
-      },
-      {
-        columnDef: 'totalPoints',
-        header: 'Points',
-        cell: (element: any) => `${element.total}`,
-      },
-    ];
-    this.displayedColumns = this.columns.map((c) => c.columnDef);
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.columnHeader = [
-      'Teams',
-      '# of Firsts',
-      '# of Seconds',
-      '# of Thirds',
-      '# of Fourths',
-      'Total Points',
-    ];
+
   }
 
-  transformedTeams(gameRankings: TeamResults[]) {
-    const transformedTeams = gameRankings
-    .sort((a, b) => b.totalPoints - a.totalPoints)
-    .map((team, index) => ({
-      position: index + 1,
-      name: team.team,
-      firsts: team.firstPlaces, 
-      seconds: team.secondPlaces,
-      thirds: team.thirdPlaces,
-      fourths: team.fourthPlaces,
-      total: team.totalPoints,
-    }));
-    return transformedTeams;
-  }
+  
 }
--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ScoreboardService } from '../services/scoreboard.service';
-import { from, map, Observable, of, timer } from 'rxjs';
-import { transformedGameResults } from '../interfaces/all-interfaces.interface';
+import { Observable } from 'rxjs';
+import { TeamResults } from '../interfaces/all-interfaces.interface';
 
 @Component({
   selector: 'app-scoreboard',
@@ -9,24 +9,95 @@ import { transformedGameResults } from '../interfaces/all-interfaces.interface';
   styleUrl: './scoreboard.component.scss'
 })
 export class ScoreboardComponent implements OnInit{
-  
-  gameRankings?: Observable<transformedGameResults[]>;
+  gameRankings?: any[];
+  dataSource?: any[];
+  contentReady = false;
   columnHeader: string[] = [];
   dataReady: Observable<boolean> | undefined;
+  displayedColumns: any;
+  columns: any[] = [];
   
   constructor(private scoreboardService: ScoreboardService) {
-    // timer(0, 30000).subscribe(() => (
-    //   this.scoreboardService.getGameResults().subscribe(gameResults => {
-    //     this.gameRankings = of(gameResults);
-    //     this.dataReady = of(true);
-    //   })
-    // ))
-    
+    setTimeout(() => {
+      this.scoreboardService.getTeamPoints().subscribe(teamResults => {
+        this.gameRankings = teamResults;
+        this.dataSource = this.transformedTeams(this.gameRankings)
+        this.contentReady = true;
+      })
+    }, 1000);
+
+    this.setColumns();
   }
 
   ngOnInit(): void {
-
-    this.columnHeader = ["Game", "First Place", "Second Place", "Third Place", "Fourth Place"]
+    this.setColumnHeader();
   }
-  
+ 
+  private setColumns() {
+    
+    this.columns = [
+      {
+        columnDef: 'position',
+        header: '#',
+        cell: (element: any) => `${element.position}`,
+      },
+      {
+        columnDef: 'name',
+        header: 'Team',
+        cell: (element: any) => `${element.name}`,
+      },
+      {
+        columnDef: 'numOfFirsts',
+        header: 'Firsts',
+        cell: (element: any) => `${element.firsts}`,
+      },
+      {
+        columnDef: 'numOfSeconds',
+        header: 'Seconds',
+        cell: (element: any) => `${element.seconds}`,
+      },
+      {
+        columnDef: 'numOfThirds',
+        header: 'Thirds',
+        cell: (element: any) => `${element.thirds}`,
+      },
+      {
+        columnDef: 'numOfFourths',
+        header: 'Fourths',
+        cell: (element: any) => `${element.fourths}`,
+      },
+      {
+        columnDef: 'totalPoints',
+        header: 'Points',
+        cell: (element: any) => `${element.total}`,
+      },
+    ];
+    this.displayedColumns = this.columns.map((c) => c.columnDef);
+  }
+
+  private setColumnHeader() {
+        this.columnHeader = [
+          'Teams',
+          '# of Firsts',
+          '# of Seconds',
+          '# of Thirds',
+          '# of Fourths',
+          'Total Points',
+        ];
+  }
+
+  transformedTeams(gameRankings: TeamResults[]) {
+    const transformedTeams = gameRankings
+    .sort((a, b) => b.totalPoints - a.totalPoints)
+    .map((team, index) => ({
+      position: index + 1,
+      name: team.team,
+      firsts: team.firstPlaces, 
+      seconds: team.secondPlaces,
+      thirds: team.thirdPlaces,
+      fourths: team.fourthPlaces,
+      total: team.totalPoints,
+    }));
+    return transformedTeams;
+  }
 }
